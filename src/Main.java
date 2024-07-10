20bd047c1ed466cc716b93bc59402a4acf771b04
Neytred <ls.ivanova91@gmail.com>
2024-07-11 02:48:07 +0900

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Additional tasks");
+        System.out.println("Additional tasks 1.2");
+        System.out.println("Task 1");
+        int a = 47;
+        int b = 13;
+        int c = a / b;
+        int d = a % b;
+        System.out.println("47/13 равно " + c + " и " + d + " в остатке.");
     }
 }
