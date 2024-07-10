acfad00c30e8a4fc4a2811cef40fcb84406380da
Neytred <ls.ivanova91@gmail.com>
2024-07-11 02:55:55 +0900

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,13 @@ public class Main {
         int c = a / b;
         int d = a % b;
         System.out.println("47/13 равно " + c + " и " + d + " в остатке.");
+
+        System.out.println("Task 2");
+        int a1 = 44;
+        int an = a1 / 10;
+        int aw = a1 % 10;
+        System.out.println("Сумма цифр числа " + a1 + " равна " + (an + aw));
+
+
     }
 }
